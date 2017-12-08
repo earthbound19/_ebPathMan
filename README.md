efde4b2f96ede40121d687800969ee743bc89c97
earthbound19 <>
2017-12-08 16:12:10 -0700

    Doppelganger integrated extensible engagement. Awesome!

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # _ebPathMan
 
 ## DESCRIPTION
-Toolset to simplify Windows PATH management.
+Toolset to simplify Windows and 'nixy PATH management.
 
 ## INSTALLATION AND USAGE
 Clone this repo and open a terminal to the path you have cloned it into, and examine the comments in and, if you think it's a good idea, run _setBinPaths.bat.
---
+++ getAllDevEnvs.sh
@@ -0,0 +1,12 @@
+# DESCRIPTION
+# Sets up a lot of temporary, customizable project paths for development. This script reads a list of project root paths from ~/projectsRootPaths.txt, and navigates to each of them in turn and invokes getDevEnv.sh for that path
+
+while read -r element
+do
+  echo $element
+done < ~/projectsRootPaths.txt
+
+
+# TANGENT: you can determine platform with the following commands:
+# uname
+# echo $OSTYPE
