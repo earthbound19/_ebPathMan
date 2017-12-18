5dc2cf69cb5b019b79c9ab83e4678e8ca7105714
earthbound19 <>
2017-12-18 15:40:03 -0700

    Attempt bug fix; paths still not adding on Mac :(

--- getDevEnv.sh
+++ getDevEnv.sh
@@ -11,29 +11,41 @@
 # -- which will permanently add the path of this script to your user ~/.bash_profile. Per the instructions in that script, you may then need to run this command to manually reload your shell with the new $PATH:
 # source ~/.bash_profile
 # AFTER THAT STEP, you may conveniently include any toolchain paths specified in any ./RELATIVEPATHS.txt file (in any directory) by invoking the following command:
-# source thisScript.sh
+# source getDevEnv.sh
+
+# DEV NOTES
+# I had the done < RELATIVEPATHS.txt line reading from that file as ./RELATIVEPATHS.txt; why I don't know: maybe it needs that on some platforms and not on others? TO DO: check on different platforms.
 
 # TO DO:
 # Have script check platform and replace stupid Windows \ with / if 'nix platform.
 
+_pwd_=$(pwd)
+
 while read -r line
 do
-	echo ~~
+	# echo --
+	# addPathToDir="$_pwd_"/"$line"
+	# echo addPathToDir value\:
+	# echo $addPathToDir
+	# echo ~~
 	echo adding path to \$PATH\:
 	echo $line
-	_pwd_=$(pwd)
-	addPathToDir="$_pwd_"/"$line"
-	echo ~~
-	echo addPathToDir value\:
-	echo $addPathToDir
-	export PATH="$PATH":"$addPathToDir"
-done < ./RELATIVEPATHS.txt
+	export PATH="$PATH:$addPathToDir"
+# MAYBE NEEDED on some platforms and not others: ./ before read file? :
+# done < ./RELATIVEPATHS.txt
+done < RELATIVEPATHS.txt
 
-echo ----------------
-echo TEMPORARY ENVIRONMENT PATHS SET DONE. The new \$PATH is\:
-echo $PATH
-echo Test the new environment for visibility of a script in the paths you just added by executing the command\:
-echo which scriptName.sh
+# echo
+# echo ----
+# echo TEMPORARY ENVIRONMENT PATHS SET DONE. The new \$PATH is\:
+# echo $PATH
+# echo
+# echo You may test the new environment for visibility of a script in the paths you just added by executing the command\:
+# echo
+# echo which scriptName.sh
+# echo
+# echo "NOTE: if the PATH to a script exists in your \$PATH (try the command 'echo \$PATH' to see if the containing folder of that script is indeed in your \$PATH), but the script does not appear via 'which scriptName.sh', there may be permission problems\, and if so ensure that all .sh files in your intended PATHS are executable."
+# echo
 
 # HISTORY
 
