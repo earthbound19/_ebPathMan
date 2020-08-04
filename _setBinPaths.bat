8956f1bf510950edcfcdad3e12ab14ede73ae77e
Alex Hall <earthbound19@users.noreply.github.com>
2020-08-04 02:03:50 -0600

    - rename script in doc.; will rename on next commit - constent use of no quotes around paths (they still get added) - warn of path length limit potential in System PATH in doc.; recommend dynamic PATH manipulation instead - echo and comment tweaks

--- _setBinPaths.bat
+++ _setBinPaths.bat
@@ -3,49 +3,46 @@
 
 :: DEPENDENCIES
 :: - modpath.exe, which is in the same directory of this script
-:: - RELATIVEPATHS.txt and ABSOLUTEPATHS.txt, as customized for any folder you place them in and run this script from.
-
-:: LICENSE: I wrote this and I dedicate this to the Public Domain.
+:: - RELATIVEPATHS.txt and ABSOLUTEPATHS.txt, as customized for any folder you place them in and run this script from, and as detailed under USAGE.
 
 :: USAGE
-:: In any folder which you wish to add to your SYSTEM PATH, place these files:
-:: - RELATIVEPATHS.txt, which is a list of directories relative to the folder which you wish to have added to your System PATH envrionment variable.
-:: - ABSOLUTEPATHS.txt, which is a list of absolute paths to directories you wish to have added to your System PATH environment variable.
-:: Both of these files should have Windows-style paths (using backslashes (\) and/or Drive indicators with colons like C:\.
+:: In any folder for which you wish to permanently add the path of that folder and/or folders inside it to your System PATH environment variable, place these files:
+:: - RELATIVEPATHS.txt, which is a list of directories inside the folder, written as paths relative to the folder, and which you want to also add permanetly to your Sytem PATH environment variable. If you list nothing in this file, only the directory that contains this file will be added permanently to the System PATH variable. See the example file of that name in this repository.
+:: - ABSOLUTEPATHS.txt, which is a list of absolute (or full) paths to directories you wish to have added to your System (permanent) PATH, one path per line of the file. See also the example file of that name in this repository.
+:: Both of these files should have Windows-style paths (using backslashes (\) and/or drive letter indicators with colons like C:\. You do not need to surround any paths with quote marks if they have spaces in them; this script does that dynamically.
+:: With those files in place and in the format described, and with this script either copied to the directory you wish to run this script from, or with this script in your System PATH, open a cmd prompt to the directory with these files, and run it:
+::    _addToSystemPATH.bat
 :: NOTES
-:: This batch will DELETE from the System PATH any paths in those files which do not actually exist, via modpath.exe
+:: - It may be preferable to not use this script, as in some (all?) situations, the permanent System variable has a hard upper length limit, and errors happen if you attempt to exceed it. You may wish to instead use getDevEnv.sh, getAllDevEnvs.sh, and/or _getDevEnv.bat, all of which dynamically alter the cmd prompt/terminal path per run, and do not permanently alter the System PATH.
+:: - This batch will DELETE from the System PATH any paths in those files which do not actually exist, via modpath.exe
+:: - Paths added dynamically to PATH are added _before_ it, so that any executables or scripts etc. added to the PATH via this script will be found by the cmd prompt system before files of the same name that may appear in other paths, and the cmd prompt will use the ones first found (they will have precedence).
+
 
 :: CODE
 ECHO OFF
-:: TO DO
-:: - figure out why it doesn't find that paths are invalid (and delete them from PATH) in relative path add loop. (Verify that that is happening also)
-:: I have contradictory notes and usage in this script: some say !CHECKPATH! (!AA!) ? doesn't work; others say %%A works. Which is it? Or is it both?
-:: - port this to a .sh script and deprecate this .bat?
-
-:: DEVELOPER NOTES
-:: Without the quote marks around path names it add directories that include spaces ( ). It will work if the first %%A doesn't have quote marks (and will mess up sorting if they do, it seems--odd).
-
 :: Wipe log for new run:
 type NUL > setPathsLog.txt
 ECHO    ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 :: START RELATIVE PATHS MANAGEMENT
 ECHO %PATH% > PATH_backup.txt
-:: fix potential line ending problems if copied files were modified on other platforms (which might have caused non-windows line endings) ; re: https://stackoverflow.com/a/27844521/1397555
+:: Fix potential line ending problems if copied files were modified on other platforms (which might have caused non-windows line endings) ; re: https://stackoverflow.com/a/27844521/1397555
 TYPE RELATIVEPATHS.txt | MORE /P > allPathsTemp_apzQT6sZHA5dm6.txt
 SET CURRDIR=%CD%
 :: Because the root of project folders is also often wanted:
 modpath.exe /add %CURRDIR%
 ECHO --ADDED to PATH: %CURRDIR% >> setPathsLog.txt
+:: Echo a blank line RE a genius breath yonder: https://stackoverflow.com/a/20696885/1397555
 ECHO[ >> setPathsLog.txt
 ECHO[
-ECHO Working from RELATIVEPATHS.txt . . .
+ECHO Will work from RELATIVEPATHS.txt . . .
 ECHO ADDS/REMOVES from RELATIVEPATHS.txt: >> setPathsLog.txt
-ECHO Added root project folder to path: %CURRDIR%
-setlocal enabledelayedexpansion
-FOR /F "delims=*" %%A IN (allPathsTemp_apzQT6sZHA5dm6.txt) DO (
+ECHO Added root project folder to PATH: %CURRDIR%
+SETLOCAL ENABLEDELAYEDEXPANSION
+:: Without the quote marks around path names it adds directories that include spaces ( ) just fine. It will work if %%A doesn't have quote marks.
+FOR /F "DELIMS=*" %%A IN (allPathsTemp_apzQT6sZHA5dm6.txt) DO (
 	:: DELETE RELATIVE PATHS that aren't found:
-	SET CHECKPATH=%CURRDIR%\%%A
-	ECHO Operating on !CHECKPATH! . . .
+	SET CHECKPATH="%CURRDIR%\%%A"
+	ECHO Will check for !CHECKPATH! . . .
 	IF NOT EXIST !CHECKPATH! modpath /del !CHECKPATH!
 	IF NOT EXIST !CHECKPATH! ECHO --NOT FOUND so removed from PATH: !CHECKPATH! >> setPathsLog.txt
 	:: ADD RELATIVE PATHS that are found:
@@ -55,14 +52,14 @@ FOR /F "delims=*" %%A IN (allPathsTemp_apzQT6sZHA5dm6.txt) DO (
 :: END RELATIVE PATHS MANAGEMENT
 
 :: START ABSOLUTE PATHS management
-:: same manipulations as for RELATIVEPATHS.txt:
+:: Same manipulations as for RELATIVEPATHS.txt:
 ECHO[ >> setPathsLog.txt
 ECHO[
-ECHO Working from ABSOLUTEPATHS.txt . . .
+ECHO Will work from ABSOLUTEPATHS.txt . . .
 ECHO ADDS/REMOVES from ABSOLUTEPATHS.txt: >> setPathsLog.txt
 TYPE ABSOLUTEPATHS.txt | MORE /P > tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
-FOR /F "delims=*" %%A IN (tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt) DO (
-	ECHO Operating on "%%A" . . .
+FOR /F "DELIMS=*" %%A IN (tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt) DO (
+	ECHO Will check for "%%A" . . .
 	IF NOT EXIST "%%A" modpath /del "%%A"
 	IF NOT EXIST "%%A" ECHO --NOT FOUND so removed from PATH: "%%A" >> setPathsLog.txt
 	IF EXIST "%%A" modpath.exe /add "%%A"
@@ -72,7 +69,6 @@ FOR /F "delims=*" %%A IN (tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt) DO (
 
 :: Delete temp files:
 DEL allPathsTemp_apzQT6sZHA5dm6.txt tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
-:: RE a genius breath yonder: https://stackoverflow.com/a/20696885/1397555
 ECHO[
 ECHO    ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
 ECHO DONE. All finds/not finds/deletes/adds to path were logged to setPathsLog.txt.
@@ -80,7 +76,7 @@ ECHO[
 
 :: DEVELOPMENT HISTORY:
 :: 2020-08-03:
-:: - per removal of GNU coreutils exes, remove lines that use those (we can get away with not using them)
+:: - per removal of GNU coreutils executable and dlls, remove lines that use those (we can get away with not using them)
 :: - use a DOS/Windows exe to convert line endings to dos (allows removal of unix2dos.exe
 :: - went through my code and realized I did things  . . . correctly :)
 :: - remove echo to stdout of info on add/remove paths
