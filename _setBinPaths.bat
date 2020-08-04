ed223aa0573d6be9767ea005fc71cd8be0bda182
Alex Hall <earthbound19@users.noreply.github.com>
2020-08-03 23:14:32 -0600

    - remove exes / dlls from this repo; - getDevEnv.sh: see dev log comments in it in diff - getAllDevEnvs.sh: improve doc comments - _setBinPaths.bat: adapt to work without removed exe/dll dependencies; other things (see dev log comments in diff) - README.md: upd. to reflect; improve doc

--- _setBinPaths.bat
+++ _setBinPaths.bat
@@ -1,88 +1,103 @@
+:: DESCRIPTION
+:: Adds custom paths to the System PATH environment variable (Windows), permanently. Must be run from a command prompt with administrative privileges to do so.
+
+:: DEPENDENCIES
+:: - modpath.exe, which is in the same directory of this script
+:: - RELATIVEPATHS.txt and ABSOLUTEPATHS.txt, as customized for any folder you place them in and run this script from.
+
+:: LICENSE: I wrote this and I dedicate this to the Public Domain.
+
+:: USAGE
+:: In any folder which you wish to add to your SYSTEM PATH, place these files:
+:: - RELATIVEPATHS.txt, which is a list of directories relative to the folder which you wish to have added to your System PATH envrionment variable.
+:: - ABSOLUTEPATHS.txt, which is a list of absolute paths to directories you wish to have added to your System PATH environment variable.
+:: Both of these files should have Windows-style paths (using backslashes (\) and/or Drive indicators with colons like C:\.
+:: NOTES
+:: This batch will DELETE from the System PATH any paths in those files which do not actually exist, via modpath.exe
+
+:: CODE
 ECHO OFF
+:: TO DO
+:: - figure out why it doesn't find that paths are invalid (and delete them from PATH) in relative path add loop. (Verify that that is happening also)
+:: I have contradictory notes and usage in this script: some say !CHECKPATH! (!AA!) ? doesn't work; others say %%A works. Which is it? Or is it both?
+:: - port this to a .sh script and deprecate this .bat?
 
-REM DESCRIPTION: _setBinPaths.bat adds custom paths to the system PATH (Windows), permanently. MUST BE RUN FROM ADMINISTRATOR command prompt. Reads from RELATIVEPATHS.txt and ABSOLUTEPATHS.txt, which must be in the directory you run this script from (and which may be used to batch set paths for various directories/projects), which files you may populate with custom paths, one per line, no semicolon. Adds the paths in those files to the system PATH variable (Windows), and optionally deletes invalid paths, using the modpath.exe utility.
+:: DEVELOPER NOTES
+:: Without the quote marks around path names it add directories that include spaces ( ). It will work if the first %%A doesn't have quote marks (and will mess up sorting if they do, it seems--odd).
 
-REM DEPENDENCIES: The modpath.exe tool which I found in the uninstall folder of ImageMagick :) and I don't know where it's from. I presume (perhaps unwisely) that it's free software.
+:: Wipe log for new run; it's beyond silly that I haven't found a way to echo nothing to a new file:
+ECHO    ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+ECHO Blanking log file . . .
+COPY NUL setPathsLog.txt
 
-REM LICENSE: I release this work to the Public Domain. 09/24/2015 09:18:03 PM -RAH
-
-REM TO DO:
-REM - figure out why it always adds the root of a project even if that's not listed (happens in relative path add loop), and fix that
-REM - figure out why it doesn't find that paths are invalid (and delete them from PATH) in relative path add loop.
-REM - port this to a .sh script and deprecate this .bat?
-
-REM =========================
-REM START RELATIVE PATHS MANAGEMENT
-
+:: START RELATIVE PATHS MANAGEMENT
 ECHO %PATH% > PATH_backup.txt
-
-REM the magic uniqify and sort commands:
-gsort RELATIVEPATHS.txt > temp_W6CyuJ5aCCRGV4.txt
-	REM NOTE: I'd had the -u flag on the next line; that messes up my intent (it does not print lines that have duplicates, it only prints unique lines. FACE PALM. ALSO: after I RT_M I learned it only detects *adjacent* duplicate lines. GEH!
-guniq temp_W6CyuJ5aCCRGV4.txt > allPathsTemp_apzQT6sZHA5dm6.txt
-REM if no trailing newline it concatenates two paths into one in PATH entry; SO:
-REM echo only a newline character trick thanks to: https://stackoverflow.com/a/3123194/1397555
-REM echo: >> allPathsTemp_apzQT6sZHA5dm6.txt
-REM fix potential line ending problems if copied files were modified on
-REM other platforms (which might have caused non-windows line endings) :
-unix2dos allPathsTemp_apzQT6sZHA5dm6.txt
-
+:: fix potential line ending problems if copied files were modified on other platforms (which might have caused non-windows line endings) ; re: https://stackoverflow.com/a/27844521/1397555
+TYPE RELATIVEPATHS.txt | MORE /P > allPathsTemp_apzQT6sZHA5dm6.txt
 SET CURRDIR=%CD%
-REM Because the root of project folders is also often wanted:
+:: Because the root of project folders is also often wanted:
 modpath.exe /add %CURRDIR%
+ECHO --ADDED to PATH: %CURRDIR% >> setPathsLog.txt
+ECHO[ >> setPathsLog.txt
+ECHO[
+ECHO Working from RELATIVEPATHS.txt . . .
+ECHO ADDS/REMOVES from RELATIVEPATHS.txt: >> setPathsLog.txt
 ECHO Added root project folder to path: %CURRDIR%
-
 setlocal enabledelayedexpansion
 FOR /F "delims=*" %%A IN (allPathsTemp_apzQT6sZHA5dm6.txt) DO (
+	:: DELETE RELATIVE PATHS that aren't found:
 	SET CHECKPATH=%CURRDIR%\%%A
-	REM For reasons I don't fully understand, I can't use %CHECKPATH% and must use !CHECKPATH! :
-	REM echo that is "!CHECKPATH!"
-	REM The next two lines remove invalid directories from your PATH; comment them out if you don't want them! :
-	IF NOT EXIST "!CHECKPATH!" modpath /del "!CHECKPATH!"
-	IF NOT EXIST "!CHECKPATH!" ECHO --Did not find and so deleted invalid directory from PATH: "!CHECKPATH!" >> setPathsLog.txt
-REM NOTE: without the quote marks on the next line, it won't add directories that include spaces ( ). It will work if the first %%A doesn't have quote marks (and will mess up sorting if they do, it seems--odd).
-	IF EXIST "!CHECKPATH!" modpath.exe /add "!CHECKPATH!"
-	IF EXIST "!CHECKPATH!" ECHO -Found relative path and added to PATH: "!CHECKPATH!"
+	ECHO Operating on !CHECKPATH! . . .
+	IF NOT EXIST !CHECKPATH! modpath /del !CHECKPATH!
+	IF NOT EXIST !CHECKPATH! ECHO --NOT FOUND so removed from PATH: !CHECKPATH! >> setPathsLog.txt
+	:: ADD RELATIVE PATHS that are found:
+	IF EXIST !CHECKPATH! modpath.exe /add !CHECKPATH!
+	IF EXIST !CHECKPATH! ECHO ---------FOUND and added to PATH: !CHECKPATH! >> setPathsLog.txt
 )
-REM END RELATIVE PATHS MANAGEMENT
-REM =========================
+:: END RELATIVE PATHS MANAGEMENT
 
-
-REM =========================
-REM START ABSOLUTE PATHS management
-
-REM same manipulations as for RELATIVEPATHS.txt:
-gsort ABSOLUTEPATHS.txt > tmp_ABSOLUTEPATHS_9nJbHt5WqaqdaS.txt
-guniq tmp_ABSOLUTEPATHS_9nJbHt5WqaqdaS.txt > tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
-echo: >> tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
-REM fix potential line ending problems in temp file that may have been modded on other platform:
-unix2dos tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
-
-REM I don't need to use !VARIABLE! in this loop, as "%%A" works for the loop variable:
+:: START ABSOLUTE PATHS management
+:: same manipulations as for RELATIVEPATHS.txt:
+ECHO[ >> setPathsLog.txt
+ECHO[
+ECHO Working from ABSOLUTEPATHS.txt . . .
+ECHO ADDS/REMOVES from ABSOLUTEPATHS.txt: >> setPathsLog.txt
+TYPE ABSOLUTEPATHS.txt | MORE /P > tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
 FOR /F "delims=*" %%A IN (tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt) DO (
+	ECHO Operating on "%%A" . . .
 	IF NOT EXIST "%%A" modpath /del "%%A"
-	IF NOT EXIST "%%A" ECHO --Did not find and so deleted invalid directory from PATH: "%%A"
-REM NOTE: without the quote marks on the next line, it won't add directories that include spaces ( ). It will work if the first %%A doesn't have quote marks (and will mess up sorting if they do, it seems--odd).
+	IF NOT EXIST "%%A" ECHO --NOT FOUND so removed from PATH: "%%A" >> setPathsLog.txt
 	IF EXIST "%%A" modpath.exe /add "%%A"
-	IF EXIST "%%A" ECHO -Found fixed path and added to PATH: "%%A"
+	IF EXIST "%%A" ECHO ---------FOUND and added to PATH: "%%A" >> setPathsLog.txt
 )
-REM END ABSOLUTE PATHS management
-REM =========================
+:: END ABSOLUTE PATHS management
 
-DEL temp_W6CyuJ5aCCRGV4.txt allPathsTemp_apzQT6sZHA5dm6.txt tmp_ABSOLUTEPATHS_9nJbHt5WqaqdaS.txt tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
+:: Delete temp files:
+DEL allPathsTemp_apzQT6sZHA5dm6.txt tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
+:: RE a genius breath yonder: https://stackoverflow.com/a/20696885/1397555
+ECHO[
+ECHO    ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+ECHO DONE. All finds/not finds/deletes/adds to path were logged to setPathsLog.txt.
+ECHO[
 
-
-REM DEVELOPMENT HISTORY:
-REM 2020-01-15 09:57 AM:
-REM - simplify log entries
-REM - Add trailing newlines to copied path list temp files instead of admonishing to have those in source files.
-REM - Change newlines to windows-style in temp files (in case they have unix newlines from edits on other platforms)
-REM - Remove vestigal temp file delete.
-REM - also sort/dedup ABSOLUTEPATHS.txt.
-REM - Don't add %CD% to paths any more; add abs. path to this project to ABSOLUTEPATHS.txt for this project instead.
-REM - used !DELAYEDVARIABLEEXPANSION! in relative paths add loop, as that seemed not to work otherwise?! For how long?!
-REM 2017 06 07 08:31:11 PM Refactored to operate on relative paths in RELATIVEPATHS.txt (as opposed to absolute) --it will prepend the current (wherever this repo is extracted) to the RELATIVEPATHS for every path mod operation. It will also load and make path change operations based on any RELATIVEPATHS.txt local to whatever directory this script is executed from (after this script itself has been added to the system or user PATH). Renamed BINPATHS.txt to RELATIVEPATHS.txt and EXTERNALPATHS to ABSOLUTEPATHS.txt
-REM 2017 05 31 10:17:59 AM severely minor tweaks
-REM 2015-12-20 Add bug comment to comments at start of file
-REM 2015-11-12 Bug fix to include necessary paths on run (had assumed so many .exes were in the same path.
-REM 2015 09 25? -- First version?
+:: DEVELOPMENT HISTORY:
+:: 2020-08-03:
+:: - per removal of GNU coreutils exes, remove lines that use those (we can get away with not using them)
+:: - use a DOS/Windows exe to convert line endings to dos (allows removal of unix2dos.exe
+:: - went through my code and realized I did things  . . . correctly :)
+:: - remove echo to stdout of info on add/remove paths
+:: - layout echo to log file for paths to be column-aligned
+:: - improve doc comments per convention
+:: 2020-01-15 09:57 AM:
+:: - simplify log entries
+:: - Add trailing newlines to copied path list temp files instead of admonishing to have those in source files.
+:: - Change newlines to windows-style in temp files (in case they have unix newlines from edits on other platforms)
+:: - Remove vestigal temp file delete.
+:: - also sort/dedup ABSOLUTEPATHS.txt.
+:: - Don't add %CD% to paths any more; add abs. path to this project to ABSOLUTEPATHS.txt for this project instead.
+:: - used !DELAYEDVARIABLEEXPANSION! in relative paths add loop, as that seemed not to work otherwise?! For how long?!
+:: 2017 06 07 08:31:11 PM Refactored to operate on relative paths in RELATIVEPATHS.txt (as opposed to absolute) --it will prepend the current (wherever this repo is extracted) to the RELATIVEPATHS for every path mod operation. It will also load and make path change operations based on any RELATIVEPATHS.txt local to whatever directory this script is executed from (after this script itself has been added to the system or user PATH). Renamed BINPATHS.txt to RELATIVEPATHS.txt and EXTERNALPATHS to ABSOLUTEPATHS.txt
+:: 2017 05 31 10:17:59 AM severely minor tweaks
+:: 2015-12-20 Add bug comment to comments at start of file
+:: 2015-11-12 Bug fix to include necessary paths on run (had assumed so many .exes were in the same path.
+:: 2015 09 25? -- First version?
--- getDevEnv.sh
+++ getDevEnv.sh
@@ -1,38 +1,44 @@
 # DESCRIPTION
 # Sets an environment path containing all paths in ./RELATIVEPATHS.txt (the file read local to whatever path your terminal is at now) for the current shell.
 
-# USAGE
-# CD to the dir containing this script, and execute the command:
-# source ./getDevEnv.sh
-#
-# SUGGESTED USAGE
-# TO MAKE THE PATH which contains this script available in your path automagically on terminal launch, from this directory first run:
-# ./addThisPathToProfile
-# -- which will permanently add the path of this script to your user ~/.bash_profile. Per the instructions in that script, you may then need to run this command to manually reload your shell with the new $PATH:
-# source ~/.bash_profile
-# AFTER THAT STEP, you may conveniently include any toolchain paths specified in any ./RELATIVEPATHS.txt file (in any directory) by invoking the following command:
-# source getDevEnv.sh
+# DEPENDENCIES
+# A Unixy environment.
 
-# TO DO:
-# Have script replace any \ with /. Modify all toolsets that use this script to define paths with / and assume this script will correct that on the fly.
+# USAGE
+# To make the path which contains this script available in your $PATH automagically on terminal launch, from this directory first run:
+#    ./addThisPathToProfile.sh
+# -- which will permanently add the path of this script to your user ~/.bash_profile. Per the instructions in that script, you may then need to run this command to manually reload your shell with the new $PATH:
+#    source ~/.bash_profile
+# After that step, you may conveniently include any toolchain paths specified in any ./RELATIVEPATHS.txt file (in any directory) by invoking the following command:
+#    source getDevEnv.sh
+# See also getAllDevEnvs.sh, which invokes this script repeatedly against a list of folders.
+# NOTE
+# This script is written with the intent that the path containing it be included in your $PATH envrionment variable via manual add to ~/.bash_profile etc. But you may also add the path containing this script manually to $PATH via the terminal (by running the command PATH=/path/to_this_script/:$PATH, or open a terminal to the path containing this script (or a copy of it), and run it from that directory.
 
 
 # CODE
+# TO DO:
+# Have script replace any \ with /. Modify all toolsets that use this script to define paths with / and assume this script will correct that on the fly.
+
 _pwd_=$(pwd)
 # Before adding subpaths via RELATIVEPATHS.txt, add the current path:
-export PATH="$PATH":"$_pwd_"
+export PATH="$_pwd_":"$PATH"
 
 # Loop over RELATIVEPATHS.txt and add paths listed in it to $PATH:
 while IFS= read -r line || [ -n "$line" ]
 do
 	# There is so much wrong with the following necessity: reverse windows backslashes, trim windows newlines so that the result isn't clobbered:
-	line=`echo $line | gsed 's/\\\\/\//g' | tr -d '\15\32'`
+	line=`echo $line | sed 's/\\\\/\//g' | tr -d '\15\32'`
 	addPathToDir="$_pwd_"/"$line"
     export PATH=$addPathToDir:$PATH
 done < RELATIVEPATHS.txt
 
 
 # HISTORY
+# 2020-08-03
+# - gsed -> sed (as dependency included in many Unixy environments or emulated environments)
+# - put custom path includes before $PATH
+# - improve doc comments
 # 2020-01-14 delete cluttered echo statements.
 # 2018-04-17 It wasn't working? Fixed (by simplifying?) and path ordering in final $PATH better.
 # 2018-02-13 Add current path before iterating over RELATIVEPATHS.txt
