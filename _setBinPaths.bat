2b289e2745650a1ae802e1c52f88db85e03c1ae1
Alex Hall <earthbound19@users.noreply.github.com>
2020-08-03 23:01:00 -0600

    finalize rework

--- _setBinPaths.bat
+++ _setBinPaths.bat
@@ -16,6 +16,7 @@
 :: This batch will DELETE from the System PATH any paths in those files which do not actually exist, via modpath.exe
 
 :: CODE
+ECHO OFF
 :: TO DO
 :: - figure out why it doesn't find that paths are invalid (and delete them from PATH) in relative path add loop. (Verify that that is happening also)
 :: I have contradictory notes and usage in this script: some say !CHECKPATH! (!AA!) ? doesn't work; others say %%A works. Which is it? Or is it both?
@@ -24,6 +25,11 @@
 :: DEVELOPER NOTES
 :: Without the quote marks around path names it add directories that include spaces ( ). It will work if the first %%A doesn't have quote marks (and will mess up sorting if they do, it seems--odd).
 
+:: Wipe log for new run; it's beyond silly that I haven't found a way to echo nothing to a new file:
+ECHO    ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+ECHO Blanking log file . . .
+COPY NUL setPathsLog.txt
+
 :: START RELATIVE PATHS MANAGEMENT
 ECHO %PATH% > PATH_backup.txt
 :: fix potential line ending problems if copied files were modified on other platforms (which might have caused non-windows line endings) ; re: https://stackoverflow.com/a/27844521/1397555
@@ -31,39 +37,56 @@ TYPE RELATIVEPATHS.txt | MORE /P > allPathsTemp_apzQT6sZHA5dm6.txt
 SET CURRDIR=%CD%
 :: Because the root of project folders is also often wanted:
 modpath.exe /add %CURRDIR%
+ECHO --ADDED to PATH: %CURRDIR% >> setPathsLog.txt
+ECHO[ >> setPathsLog.txt
+ECHO[
+ECHO Working from RELATIVEPATHS.txt . . .
+ECHO ADDS/REMOVES from RELATIVEPATHS.txt: >> setPathsLog.txt
 ECHO Added root project folder to path: %CURRDIR%
 setlocal enabledelayedexpansion
 FOR /F "delims=*" %%A IN (allPathsTemp_apzQT6sZHA5dm6.txt) DO (
+	:: DELETE RELATIVE PATHS that aren't found:
 	SET CHECKPATH=%CURRDIR%\%%A
-	REM The next two lines remove invalid directories from your PATH; comment them out if you don't want them! :
-	IF NOT EXIST "!CHECKPATH!" modpath /del "!CHECKPATH!"
-	IF NOT EXIST "!CHECKPATH!" ECHO --Did not find and so deleted invalid directory from PATH: "!CHECKPATH!" >> setPathsLog.txt
-:: NOTE: without the quote marks on the next line, it won't add directories that include spaces ( ). It will work if the first %%A doesn't have quote marks (and will mess up sorting if they do, it seems--odd).
-	IF EXIST "!CHECKPATH!" modpath.exe /add "!CHECKPATH!"
-	IF EXIST "!CHECKPATH!" ECHO -Found relative path and added to PATH: "!CHECKPATH!"
+	ECHO Operating on !CHECKPATH! . . .
+	IF NOT EXIST !CHECKPATH! modpath /del !CHECKPATH!
+	IF NOT EXIST !CHECKPATH! ECHO --NOT FOUND so removed from PATH: !CHECKPATH! >> setPathsLog.txt
+	:: ADD RELATIVE PATHS that are found:
+	IF EXIST !CHECKPATH! modpath.exe /add !CHECKPATH!
+	IF EXIST !CHECKPATH! ECHO ---------FOUND and added to PATH: !CHECKPATH! >> setPathsLog.txt
 )
 :: END RELATIVE PATHS MANAGEMENT
 
 :: START ABSOLUTE PATHS management
 :: same manipulations as for RELATIVEPATHS.txt:
-TYPE RELATIVEPATHS.txt | MORE /P > tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
+ECHO[ >> setPathsLog.txt
+ECHO[
+ECHO Working from ABSOLUTEPATHS.txt . . .
+ECHO ADDS/REMOVES from ABSOLUTEPATHS.txt: >> setPathsLog.txt
+TYPE ABSOLUTEPATHS.txt | MORE /P > tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
 FOR /F "delims=*" %%A IN (tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt) DO (
+	ECHO Operating on "%%A" . . .
 	IF NOT EXIST "%%A" modpath /del "%%A"
-	IF NOT EXIST "%%A" ECHO --Did not find and so deleted invalid directory from PATH: "%%A"
+	IF NOT EXIST "%%A" ECHO --NOT FOUND so removed from PATH: "%%A" >> setPathsLog.txt
 	IF EXIST "%%A" modpath.exe /add "%%A"
-	IF EXIST "%%A" ECHO -Found fixed path and added to PATH: "%%A"
+	IF EXIST "%%A" ECHO ---------FOUND and added to PATH: "%%A" >> setPathsLog.txt
 )
 :: END ABSOLUTE PATHS management
 
 :: Delete temp files:
 DEL allPathsTemp_apzQT6sZHA5dm6.txt tmp_ABSOLUTEPATHS_29HN7TMFWKHYPj.txt
-
+:: RE a genius breath yonder: https://stackoverflow.com/a/20696885/1397555
+ECHO[
+ECHO    ~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
+ECHO DONE. All finds/not finds/deletes/adds to path were logged to setPathsLog.txt.
+ECHO[
 
 :: DEVELOPMENT HISTORY:
-:: 2020-07-30 03:36 PM:
+:: 2020-08-03:
 :: - per removal of GNU coreutils exes, remove lines that use those (we can get away with not using them)
 :: - use a DOS/Windows exe to convert line endings to dos (allows removal of unix2dos.exe
-:: - added notes observing contradictory claims between !VARIABLE! working and %%VARIABLE working; will test that all . . .
+:: - went through my code and realized I did things  . . . correctly :)
+:: - remove echo to stdout of info on add/remove paths
+:: - layout echo to log file for paths to be column-aligned
 :: 2020-01-15 09:57 AM:
 :: - simplify log entries
 :: - Add trailing newlines to copied path list temp files instead of admonishing to have those in source files.
